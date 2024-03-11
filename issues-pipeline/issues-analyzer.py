1ca0e48d4d518d70940421699d07879c8af463bf
Saisha Shetty <saisha892001@gmail.com>
2024-03-10 22:33:54 -0700

    test

---
+++ issues-pipeline/issues-analyzer.py
@@ -0,0 +1,71 @@
+import csv
+from github import Github
+import os
+import sys
+from datetime import datetime
+from datetime import timezone
+#from dotenv import load_dotenv  
+sys.path.append('..')
+
+from toxicity_detector.toxicity_detector import classify_sentence, nltk_classify_sentence
+#load_dotenv()  # Load environment variables from .env file
+
+def extract_issues_and_comments(repository_url, output_file, github_token):
+    # Create a Github instance
+    g = Github(github_token)
+
+    # Get the repository
+    repo = g.get_repo(repository_url)
+
+    # Open a CSV file for writing
+    csv_file_path = 'output_' + repository_url.replace("/", "_") + '.csv'
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        # Write header row
+        csv_writer.writerow(['Issue ID', 'Title', 'Body', 'State', 'Created at', 'Closed at', 'Original Author', 'Comment Author', 'Comment', 'Commented at', 'Toxicity', 'NLTK Sentiment'])
+
+        try:
+            for issue in repo.get_issues(state='all'):
+                # Write issue information to CSV
+                csv_writer.writerow([issue.number, issue.title, issue.body, issue.state, issue.created_at, issue.closed_at, issue.user.login, '', '', '', '', ''])
+
+                # Check if the user already exists before adding a new entry
+                user_exists = False
+                for user in issue.user.login:
+                    user_exists = True
+                    comment = issue.body if issue.body is not None else "There is no body for this issue."
+                    csv_writer.writerow(['', '', '', '', '', '', '', user, comment, issue.created_at, classify_sentence(comment), nltk_classify_sentence(comment)])
+
+                if not user_exists:
+                    comment = issue.body if issue.body is not None else "There is no body for this issue."
+                    csv_writer.writerow(['', '', '', '', '', '', '', issue.user.login, comment, issue.created_at, classify_sentence(comment), nltk_classify_sentence(comment)])
+
+                # Write comments information to CSV
+                for comment in issue.get_comments():
+                    csv_writer.writerow(['', '', '', '', '', '', '', comment.user.login, comment.body, comment.created_at, classify_sentence(comment.body), nltk_classify_sentence(comment.body)])
+
+        except Exception as e:
+            print(e)
+            print(f"Error occurred while processing the issue {issue.number}. Skipping to the next issue.")
+            pass
+
+if __name__ == "__main__":
+    # Replace 'username/repo' with the actual repository path
+    repository_url = 'google/guava'
+
+    # Specify the output file
+    output_file = 'output_' + repository_url.replace("/", "_") + '.csv'
+
+    # Add your GitHub token here
+    github_token = os.environ.get('GITHUB_TOKEN')
+    print(f"Repository URL: {repository_url}")
+    print(f"GitHub Token: {github_token}")
+
+    # Create a Github instance
+    g = Github(github_token)
+
+    # Get the repository
+    repo = g.get_repo(repository_url)
+
+    extract_issues_and_comments(repository_url, output_file, github_token)
